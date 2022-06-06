c38325a20a6e5f65d7d48bf7f5a246b4b9c52775
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2022-06-06 02:37:37 +0200

    initial version (#1)
    * initial commit
    
    * added bash upfront
    
    * corrected SHELL error and pushed bash down, again
    
    * Reverse package installation and binary fetch
    
    * create test case for buildx ci

---
+++ scripts/common.sh
@@ -0,0 +1,107 @@
+# shellcheck shell=bash
+[[ "$DEBUG" ]] && set -x
+# https://devhints.io/bash
+set -euo pipefail # strict
+IFS=$'\n\t'
+
+traperr() {
+	echo "ERROR: ${BASH_SOURCE[1]} at about ${BASH_LINENO[0]}"
+}
+
+case $(readlink /proc/$$/exe) in
+/bin/bash)
+	set -o errtrace
+	trap traperr ERR
+	;;
+esac
+
+function checkDir() {
+	local dir="${1:?'Please specify a directory!'}"
+	if [ ! -d "$dir" ]; then
+		echo "$dir not found"
+		exit 2 # ENOENT No such file or directory
+	fi
+}
+
+function checkFile() {
+	local file="${1:?'Please specify a file!'}"
+	if [ ! -s "$file" ]; then
+		echo "$file not found"
+		exit 2 # ENOENT No such file or directory
+	fi
+}
+
+function printHeading() {
+	local padlimit='100'
+	local text="$*"
+	local pad
+	# shellcheck disable=SC2183
+	pad="$(printf '%*s' "$padlimit")"
+	pad="${pad// /=}"
+	printf '%s' "$text "
+	printf '%*.*s\n' 0 $((padlimit - "${#text}")) "$pad"
+}
+
+# shellcheck disable=SC2068
+function runCMD() {
+	echo 'λ' $@
+	$@
+}
+
+function prepareSSH() {
+	printHeading 'preparing ssh environment'
+	local privateKeyData="${1:-"${SSH_KEY:?'Need private key!'}"}"
+	local privateKeyName="${2:-id_ed25519}"
+	mkdir -p ~/.ssh
+	echo "$privateKeyData" >~/.ssh/"$privateKeyName"
+	chmod 0400 ~/.ssh/"$privateKeyName"
+}
+
+function prepareGIT() {
+	printHeading 'preparing git environment'
+	local repo_path="${1:?'Please specify a git repository!'}"
+	local git_mail="${2:-"${MAILBOX:?'Please specify your mail address!'}"}"
+	local git_name="${3:-"${GITNAME:-automaton @ http://concourse-ci.org}"}"
+	cd "$repo_path" || (echo "$repo_path does not exist" && exit 2) # ENOENT
+	runCMD git config --global --add safe.directory "$(pwd)"
+	runCMD git config --global user.email "$git_mail"
+	runCMD git config --global user.name "$git_name"
+}
+
+function cloneGitRepo() {
+	printHeading 'cloning git repository'
+	local clonerepo="${1:?'Please specify source repository!'}"
+	local dest_repo="${2:?'Please specify target repository!'}"
+	local user_name="${3:-"${HOME##*/}"}" # either root or app
+	runCMD sudo chown -R "$user_name" "$dest_repo"
+	runCMD git clone "$clonerepo" "$dest_repo"
+}
+
+function initBranch() {
+	printHeading 'preparing ophaned git branch'
+	local branchName="${1:?'Please specify a branch name!'}"
+	runCMD git switch --orphan "$branchName"
+	runCMD git rm --cached -r . || true
+	runCMD git clean -df .??* . || true
+	runCMD git commit --allow-empty -m "init"
+}
+
+function commitAllFiles() {
+	printHeading 'committing to repository'
+	local message="${1:?'Please specify a commit message!'}"
+	runCMD git add .
+	runCMD git commit -m "$message"
+}
+
+function tracerouteSSH() {
+	printHeading 'tracing route to ssh port of nodes'
+	local sourceFile="${1:?'Please specify a source file!'}"
+	local portNumber="${2:-22}"
+	local searchPatt='(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+	mkfifo addresses
+	grep -E -o "$searchPatt" "$sourceFile" >addresses &
+	while IFS= read -r ipv4; do
+		runCMD mtr --report-wide --no-dns --tcp --port "$portNumber" "$ipv4"
+	done <addresses
+	rm addresses
+}
---
+++ scripts/k0sctl-handler.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+# shellcheck source=common.sh
+source "${0%/*}"/common.sh
+
+printHeading 'k0sctl handler'
+case "${K0SCTL_CMD_NAME-version}" in
+version)
+	runCMD k0sctl version
+	exit 0 # buildx test
+	;;
+esac
+
+printHeading 'preparing environment'
+env | grep K0SCTL # show the vars that be
+prepareSSH "$SSH_KEY"
+
+CFG="$(pwd)/${K0SCTL_DIR_CFG:?}/${K0SCTL_CFG_PATH:?}"
+checkFile "$CFG"
+
+LOG="$(pwd)/${K0SCTL_DIR_LOG:?}"
+checkDir "$LOG"
+
+BAK="$(pwd)/${K0SCTL_DIR_BAK:?}"
+RES="$(pwd)/${K0SCTL_DIR_RES:?}"
+
+printHeading 'managing cluster'
+cd "$HOME" || exit 2 # ENOENT
+started="$(date +%F-%H-%M-%S)"
+case "${K0SCTL_CMD_NAME:?}" in
+install)
+	# shellcheck disable=SC2086
+	runCMD k0sctl apply --config "$CFG" $K0SCTL_CMD_ARGS
+	;;
+uninstall)
+	# shellcheck disable=SC2086
+	runCMD k0sctl reset --config "$CFG" --force $K0SCTL_CMD_ARGS
+	;;
+backup)
+	checkDir "$BAK"
+	checkDir "$RES"
+	# shellcheck disable=SC2086
+	runCMD k0sctl backup --config "$CFG" $K0SCTL_CMD_ARGS
+	;;
+restore)
+	checkDir "$RES"
+	archive="$RES/${PREFIX_BAK}_latest"
+	checkFile "$archive"
+	# shellcheck disable=SC2086
+	runCMD k0sctl apply --config "$CFG" --restore-from="${archive}" $K0SCTL_CMD_ARGS
+	;;
+*)
+	exit 1 # EPERM Operation not permitted
+	;;
+esac
+
+printHeading 'saving logfile'
+runCMD sudo mv ~/.cache/k0sctl/k0sctl.log "$LOG/$started-$K0SCTL_CMD_NAME.$SUFFIX_LOG"
+
+case "$K0SCTL_CMD_NAME" in
+backup)
+	USR="${HOME##*/}"
+	printHeading 'saving backup archive'
+	cloneGitRepo "$RES" "$BAK" "$USR"
+	prepareGIT "$BAK" "$MAILBOX"
+	runCMD git checkout "${K0SCTL_DIR_BAK}"
+	latest="${PREFIX_BAK}_latest"
+	mapfile -t archives < <(find "$HOME" -maxdepth 1 -name "${PREFIX_BAK}*${SUFFIX_BAK}")
+	for archiveHome in "${archives[@]}"; do
+		checkFile "$archiveHome"
+		archive="${archiveHome##*/}"
+		runCMD mv -n "$archiveHome" -t "$BAK"
+		runCMD ln -sb "$archive" -T "$latest"
+		commitAllFiles "$archive saved as $latest"
+	done
+	;;
+esac
