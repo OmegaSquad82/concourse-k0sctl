a97ed9deffe231b18578eb4d6acbd490b849d23e
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2022-06-06 16:13:30 +0200

    Installed an EXIT trap to save k0sctl's logfile

--- scripts/k0sctl-handler.sh
+++ scripts/k0sctl-handler.sh
@@ -12,9 +12,9 @@ esac
 
 printHeading 'preparing environment'
 case "${DISABLE_TELEMETRY:-false}" in
-true)
-	;;
-*) DISABLE_TELEMETRY=false
+true) ;;
+*)
+	DISABLE_TELEMETRY=false
 	;;
 esac
 env | grep -E '(K0SCTL|TELEMETRY)'
@@ -30,9 +30,18 @@ BAK="$(pwd)/${K0SCTL_DIR_BAK:=backup}"
 RES="$(pwd)/${K0SCTL_DIR_RES:-restore}"
 latest="${PREFIX_BAK:=k0s_backup}_latest"
 
-printHeading 'managing cluster'
 cd "$HOME" || exit 2 # ENOENT
 started="$(date +%F-%H-%M-%S)"
+function finish() {
+	local logfile="$LOG/$started-$K0SCTL_CMD_NAME.${SUFFIX_LOG:-log}"
+	if [ -s "$logfile" ]; then
+		printHeading 'saving logfile'
+		runCMD mv ~/.cache/k0sctl/k0sctl.log "$logfile"
+	fi
+}
+trap finish EXIT
+
+printHeading 'managing cluster'
 case "$K0SCTL_CMD_NAME" in
 install)
 	if [ -d "$RES" ] && [ -s "$RES/${latest}" ]; then
@@ -52,20 +61,8 @@ backup)
 	assertDir "$RES"
 	# shellcheck disable=SC2086
 	runCMD k0sctl backup --config "$CFG" ${K0SCTL_CMD_ARGS:-}
-	;;
-*)
-	exit 1 # EPERM Operation not permitted
-	;;
-esac
-
-printHeading 'saving logfile'
-runCMD mv ~/.cache/k0sctl/k0sctl.log "$LOG/$started-$K0SCTL_CMD_NAME.${SUFFIX_LOG:-log}"
-
-case "$K0SCTL_CMD_NAME" in
-backup)
-	USR="${HOME##*/}"
 	printHeading 'saving backup archive'
-	cloneGitRepo "$RES" "$BAK" "$USR"
+	cloneGitRepo "$RES" "$BAK" "${HOME##*/}"
 	prepareGIT "$BAK" "$MAILBOX"
 	runCMD git checkout "${K0SCTL_DIR_BAK}"
 	mapfile -t archives < <(find "$HOME" -maxdepth 1 -name "${PREFIX_BAK}*${SUFFIX_BAK:-tar.gz}")
@@ -76,5 +73,8 @@ backup)
 		runCMD ln -sb "$archive" -T "$latest"
 		commitAllFiles "$archive saved as $latest"
 	done
+	;;
+*)
+	exit 1 # EPERM Operation not permitted
 	;;
 esac
