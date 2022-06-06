94f88aa5205c050359db1a07bbc072ac685d6288
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2022-06-06 16:59:23 +0200

    As stated in the documentation restore's 4 desasters https://github.c… (#8)
    * As stated in the documentation restore's 4 desasters https://github.com/k0sproject/k0sctl/blob/main/README.md#k0sctl-backup--restore
    and the initial PR's contents indicate that it skips restore when it encounters an already running k0s installation. https://github.com/k0sproject/k0sctl/pull/149
    
    * Use latest variable to obtain the backup archive
    
    * Installed an EXIT trap to save k0sctl's logfile
    
    * allow reset in case when the pipeline never ran
    
    * Remove the simplest of git functions
    
    * Serialize Jobs calling k0sctl against each other
    
    * Fix logical error where it checked the target...
    
    * Exposed K0SCTL_LOG_PATH as it's default log path.

--- scripts/k0sctl-handler.sh
+++ scripts/k0sctl-handler.sh
@@ -12,9 +12,9 @@ esac
 
 printHeading 'preparing environment'
 case "${DISABLE_TELEMETRY:-false}" in
-true)
-	;;
-*) DISABLE_TELEMETRY=false
+true) ;;
+*)
+	DISABLE_TELEMETRY=false
 	;;
 esac
 env | grep -E '(K0SCTL|TELEMETRY)'
@@ -30,13 +30,27 @@ BAK="$(pwd)/${K0SCTL_DIR_BAK:=backup}"
 RES="$(pwd)/${K0SCTL_DIR_RES:-restore}"
 latest="${PREFIX_BAK:=k0s_backup}_latest"
 
-printHeading 'managing cluster'
 cd "$HOME" || exit 2 # ENOENT
 started="$(date +%F-%H-%M-%S)"
+function finish() {
+	local logfile="$LOG/$started-$K0SCTL_CMD_NAME.${SUFFIX_LOG:-log}"
+	if [ -s "${K0SCTL_LOG_PATH:="$HOME/.cache/k0sctl/k0sctl.log"}" ]; then
+		printHeading 'saving logfile'
+		runCMD mv "$K0SCTL_LOG_PATH" "$logfile"
+	fi
+}
+trap finish EXIT
+
+printHeading 'managing cluster'
 case "$K0SCTL_CMD_NAME" in
 install)
-	# shellcheck disable=SC2086
-	runCMD k0sctl apply --config "$CFG" ${K0SCTL_CMD_ARGS:-}
+	if [ -d "$RES" ] && [ -s "$RES/${latest}" ]; then
+		# shellcheck disable=SC2086
+		runCMD k0sctl apply --config "$CFG" --restore-from="$RES/${latest}" ${K0SCTL_CMD_ARGS:-}
+	else
+		# shellcheck disable=SC2086
+		runCMD k0sctl apply --config "$CFG" ${K0SCTL_CMD_ARGS:-}
+	fi
 	;;
 uninstall)
 	# shellcheck disable=SC2086
@@ -47,26 +61,8 @@ backup)
 	assertDir "$RES"
 	# shellcheck disable=SC2086
 	runCMD k0sctl backup --config "$CFG" ${K0SCTL_CMD_ARGS:-}
-	;;
-restore)
-	assertDir "$RES"
-	assertFile "$RES/${latest}"
-	# shellcheck disable=SC2086
-	runCMD k0sctl apply --config "$CFG" --restore-from="$RES/${latest}" ${K0SCTL_CMD_ARGS:-}
-	;;
-*)
-	exit 1 # EPERM Operation not permitted
-	;;
-esac
-
-printHeading 'saving logfile'
-runCMD mv ~/.cache/k0sctl/k0sctl.log "$LOG/$started-$K0SCTL_CMD_NAME.${SUFFIX_LOG:-log}"
-
-case "$K0SCTL_CMD_NAME" in
-backup)
-	USR="${HOME##*/}"
 	printHeading 'saving backup archive'
-	cloneGitRepo "$RES" "$BAK" "$USR"
+	runCMD git clone "$RES" "$BAK"
 	prepareGIT "$BAK" "$MAILBOX"
 	runCMD git checkout "${K0SCTL_DIR_BAK}"
 	mapfile -t archives < <(find "$HOME" -maxdepth 1 -name "${PREFIX_BAK}*${SUFFIX_BAK:-tar.gz}")
@@ -75,7 +71,11 @@ backup)
 		archive="${archiveHome##*/}"
 		runCMD mv -n "$archiveHome" -t "$BAK"
 		runCMD ln -sb "$archive" -T "$latest"
-		commitAllFiles "$archive saved as $latest"
+		runCMD git add .
+		runCMD git commit -m "$archive saved as $latest"
 	done
+	;;
+*)
+	exit 1 # EPERM Operation not permitted
 	;;
 esac
