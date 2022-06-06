3a04a7ba20e0541f8a41336cb2ceaca481046e1e
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2022-06-06 14:21:48 +0200

    3 parameters written in the pipeline instead of in the script (#6)
    * Remove parameters from k0sctl_task
    
    * Rename checkDir and checkFile to more expressive
    assertDir and assertfile thus indicating they exit.
    
    * Incorporate default values, use renamed assertDir
    and assertFile functions, set Telemetry variable,
    expose SSH_TYPE
    
    * Set PREFIX_BAK and K0SCTL_CMD_ARGS for later
    
    * Set K0SCTL_DIR_BAK for git operations
    
    * Alway insert empty value for K0SCTL_CMD_ARGS
    
    * Expose a mail address for commits to backup
    
    * Remove the branch parameter from the put-steps,
    as it creates ugly outputs on the dependency graphs
    where same commits are displayed in different rows
    because they have been pulled without and pushed
    with separate branch information. (although that is
    not true, as the resource has the branch parameter)

--- scripts/k0sctl-handler.sh
+++ scripts/k0sctl-handler.sh
@@ -3,7 +3,7 @@
 source "${0%/*}"/common.sh
 
 printHeading 'k0sctl handler'
-case "${K0SCTL_CMD_NAME-version}" in
+case "${K0SCTL_CMD_NAME:-version}" in
 version)
 	runCMD k0sctl version
 	exit 0 # buildx test
@@ -11,42 +11,48 @@ version)
 esac
 
 printHeading 'preparing environment'
-env | grep K0SCTL # the vars that be
-prepareSSH "$SSH_KEY"
+case "${DISABLE_TELEMETRY:-false}" in
+true)
+	;;
+*) DISABLE_TELEMETRY=false
+	;;
+esac
+env | grep -E '(K0SCTL|TELEMETRY)'
+prepareSSH "$SSH_KEY" "${SSH_TYPE:-id_ed25519}"
 
-CFG="$(pwd)/${K0SCTL_DIR_CFG:?}/${K0SCTL_CFG_PATH:?}"
-checkFile "$CFG"
+CFG="$(pwd)/${K0SCTL_DIR_CFG:-config}/${K0SCTL_CFG_PATH:-k0sctl.yaml}"
+assertFile "$CFG"
 
-LOG="$(pwd)/${K0SCTL_DIR_LOG:?}"
-checkDir "$LOG"
+LOG="$(pwd)/${K0SCTL_DIR_LOG:-auditlog}"
+assertDir "$LOG"
 
-BAK="$(pwd)/${K0SCTL_DIR_BAK:?}"
-RES="$(pwd)/${K0SCTL_DIR_RES:?}"
+BAK="$(pwd)/${K0SCTL_DIR_BAK:=backup}"
+RES="$(pwd)/${K0SCTL_DIR_RES:-restore}"
+latest="${PREFIX_BAK:=k0s_backup}_latest"
 
 printHeading 'managing cluster'
 cd "$HOME" || exit 2 # ENOENT
 started="$(date +%F-%H-%M-%S)"
-case "${K0SCTL_CMD_NAME:?}" in
+case "$K0SCTL_CMD_NAME" in
 install)
 	# shellcheck disable=SC2086
-	runCMD k0sctl apply --config "$CFG" $K0SCTL_CMD_ARGS
+	runCMD k0sctl apply --config "$CFG" ${K0SCTL_CMD_ARGS:-}
 	;;
 uninstall)
 	# shellcheck disable=SC2086
-	runCMD k0sctl reset --config "$CFG" --force $K0SCTL_CMD_ARGS
+	runCMD k0sctl reset --config "$CFG" --force ${K0SCTL_CMD_ARGS:-}
 	;;
 backup)
-	checkDir "$BAK"
-	checkDir "$RES"
+	assertDir "$BAK"
+	assertDir "$RES"
 	# shellcheck disable=SC2086
-	runCMD k0sctl backup --config "$CFG" $K0SCTL_CMD_ARGS
+	runCMD k0sctl backup --config "$CFG" ${K0SCTL_CMD_ARGS:-}
 	;;
 restore)
-	checkDir "$RES"
-	archive="$RES/${PREFIX_BAK}_latest"
-	checkFile "$archive"
+	assertDir "$RES"
+	assertFile "$RES/${latest}"
 	# shellcheck disable=SC2086
-	runCMD k0sctl apply --config "$CFG" --restore-from="${archive}" $K0SCTL_CMD_ARGS
+	runCMD k0sctl apply --config "$CFG" --restore-from="$RES/${latest}" ${K0SCTL_CMD_ARGS:-}
 	;;
 *)
 	exit 1 # EPERM Operation not permitted
@@ -54,7 +60,7 @@ restore)
 esac
 
 printHeading 'saving logfile'
-runCMD mv ~/.cache/k0sctl/k0sctl.log "$LOG/$started-$K0SCTL_CMD_NAME.$SUFFIX_LOG"
+runCMD mv ~/.cache/k0sctl/k0sctl.log "$LOG/$started-$K0SCTL_CMD_NAME.${SUFFIX_LOG:-log}"
 
 case "$K0SCTL_CMD_NAME" in
 backup)
@@ -63,10 +69,9 @@ backup)
 	cloneGitRepo "$RES" "$BAK" "$USR"
 	prepareGIT "$BAK" "$MAILBOX"
 	runCMD git checkout "${K0SCTL_DIR_BAK}"
-	latest="${PREFIX_BAK}_latest"
-	mapfile -t archives < <(find "$HOME" -maxdepth 1 -name "${PREFIX_BAK}*${SUFFIX_BAK}")
+	mapfile -t archives < <(find "$HOME" -maxdepth 1 -name "${PREFIX_BAK}*${SUFFIX_BAK:-tar.gz}")
 	for archiveHome in "${archives[@]}"; do
-		checkFile "$archiveHome"
+		assertFile "$archiveHome"
 		archive="${archiveHome##*/}"
 		runCMD mv -n "$archiveHome" -t "$BAK"
 		runCMD ln -sb "$archive" -T "$latest"
