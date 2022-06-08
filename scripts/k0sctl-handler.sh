16ec7284985ea955f635b58ad5fd340ed02f894e
OmegaSquad82 <34405234+OmegaSquad82@users.noreply.github.com>
2022-06-08 23:49:39 +0200

    Pull git code out of k0sctl-handler.sh, (#14)
    * Pull git code out of k0sctl-handler.sh,
    prepend all envs with K0SCTL_ for
    consistency. Don't you start over
    CFG_PATH and DIR_CFG. Later?
    
    * Need to check dir change.
    
    * Fix mailbox and transfer original message
    
    * hand over backup directory to shell_task

--- scripts/k0sctl-handler.sh
+++ scripts/k0sctl-handler.sh
@@ -17,8 +17,8 @@ true) ;;
 	DISABLE_TELEMETRY=false
 	;;
 esac
-env | grep -E '(K0SCTL|TELEMETRY)'
-prepareSSH "$SSH_KEY" "${SSH_TYPE:-id_ed25519}"
+env | grep -E '(K0SCTL|TELEMETRY)' | grep -v 'SSH'
+prepareSSH "$K0SCTL_SSH_KEY" "${K0SCTL_SSH_TYPE:-id_ed25519}"
 
 CFG="$(pwd)/${K0SCTL_DIR_CFG:-config}/${K0SCTL_CFG_PATH:-k0sctl.yaml}"
 assertFile "$CFG"
@@ -28,12 +28,12 @@ assertDir "$LOG"
 
 BAK="$(pwd)/${K0SCTL_DIR_BAK:=backup}"
 RES="$(pwd)/${K0SCTL_DIR_RES:-restore}"
-latest="${PREFIX_BAK:=k0s_backup}_latest"
+latest="${K0SCTL_PREFIX_BAK:=k0s_backup}_latest"
 
 cd "$HOME" || exit 2 # ENOENT
 started="$(date +%F-%H-%M-%S)"
 function finish() {
-	local logfile="$LOG/$started-$K0SCTL_CMD_NAME.${SUFFIX_LOG:-log}"
+	local logfile="$LOG/$started-$K0SCTL_CMD_NAME.${K0SCTL_SUFFIX_LOG:-log}"
 	if [ -s "${K0SCTL_LOG_PATH:="$HOME/.cache/k0sctl/k0sctl.log"}" ]; then
 		printHeading 'saving logfile'
 		runCMD mv "$K0SCTL_LOG_PATH" "$logfile"
@@ -62,17 +62,14 @@ backup)
 	# shellcheck disable=SC2086
 	runCMD k0sctl backup --config "$CFG" ${K0SCTL_CMD_ARGS:-}
 	printHeading 'saving backup archive'
-	runCMD git clone "$RES" "$BAK"
-	prepareGIT "$BAK" "$MAILBOX"
-	runCMD git checkout "${K0SCTL_DIR_BAK}"
-	mapfile -t archives < <(find "$HOME" -maxdepth 1 -name "${PREFIX_BAK}*${SUFFIX_BAK:-tar.gz}")
+	cd "$BAK" || (echo "$BAK does not exist" && exit 2) # ENOENT
+	mapfile -t archives < <(find "$HOME" -maxdepth 1 -name "${K0SCTL_PREFIX_BAK}*${K0SCTL_SUFFIX_BAK:-tar.gz}")
 	for archiveHome in "${archives[@]}"; do
 		assertFile "$archiveHome"
 		archive="${archiveHome##*/}"
 		runCMD mv -n "$archiveHome" -t "$BAK"
 		runCMD ln -sb "$archive" -T "$latest"
-		runCMD git add .
-		runCMD git commit -m "$archive saved as $latest"
+		echo "$archive saved as $latest" >.message
 	done
 	;;
 *)
